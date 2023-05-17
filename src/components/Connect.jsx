095c0f20a0281ddd35c5966f99e5dec48d848fa7
Irfan Saeed Khan <irfansaeedkhan@protonmail.com>
2023-05-17 20:03:50 +0500

    twiiter icon added

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import telegram from "/telegram.png";
 import discord from "/discord.png";
+import twitter from "/twitter.png";
 
 const Connect = () => {
   return (
@@ -9,16 +10,38 @@ const Connect = () => {
         <h1 className="text-[#494866] text-4xl font-bold text-center mt-5 mb-10">
           CONNECT WITH US
         </h1>
-        <div className="flex flex-col lg:flex-row items-center justify-center text-center gap-4 margin-0 text-[#494866]">
-          <div className="w-20  lg:w-28 flex flex-col ">
-            <img className="h-fit rounded-lg" src={telegram} alt="Logo" />
-            telegram
-          </div>
+        <div className="flex items-center justify-center text-center gap-4 margin-0 text-[#494866]">
+          <a
+            href="https://t.me/HomerGem"
+            target="_blank"
+            className="w-16  lg:w-24 flex flex-col "
+          >
+            <img
+              className="h-fit rounded-lg"
+              src={telegram}
+              alt="Telegram Logo"
+            />
+          </a>
 
-          <div className="w-20 lg:w-28 flex flex-col">
-            <img className="h-fit rounded-lg" src={discord} alt="Logo" />
-            discord
-          </div>
+          <a
+            href="https://discord.gg/z5xPxmEY"
+            target="_blank"
+            className="w-16 lg:w-24 flex flex-col"
+          >
+            <img
+              className="h-fit rounded-lg"
+              src={discord}
+              alt="Discord Logo"
+            />
+          </a>
+
+          <a href="#" className="w-16 lg:w-24 flex flex-col">
+            <img
+              className="h-fit rounded-lg"
+              src={twitter}
+              alt="Twitter Logo"
+            />
+          </a>
         </div>
       </div>
     </div>
